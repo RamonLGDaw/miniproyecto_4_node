57ed299857d71a90304f845d8ec490aa8b3a28c2
RamonLGDaw <ramonlg.daw@gmail.com>
2023-12-04 19:56:06 +0100

    variables de entorno

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const express = require('express')
 const app = express()
-const puerto = process.env.PORT || 3000;
 const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+//Pruebas para averiguar porque no funcionaban las putas variables de entorno... al final es que USER debe ser una palabra 'reservada' o algo asi
+
+ const puerto = process.env.PORT || 3000;
+// const nombre = process.env.NOM;
+// const password = process.env.PASSWORD;
+// const bdName = process.env.NAME;
+
+// console.log(`contenido de la variable nombre: ${nombre}`);
+// console.log(`que tipo de variable es nombre?? ==> ${typeof nombre}`)
+// console.log(`contenido de la variable password: ${password}`);
+// console.log(`que tipo de variable es nombre?? ==> ${typeof password}`)
+// console.log(`contenido de la variable BASE DE DATOS: ${bdName}`);
+// console.log(`que tipo de variable es nombre?? ==> ${typeof bdName}`)
 
 
 //Conexion a base de datos
-const user = 'usuarioGeneral';
-const password = 'contrasenya';
-const dbname = 'persona'
-const uri = `mongodb+srv://${user}:${password}@cluster0.n1h1nz9.mongodb.net/${dbname}?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.NOM}:${process.env.PASSWORD}@cluster0.n1h1nz9.mongodb.net/${process.env.NAME}?retryWrites=true&w=majority`;
 
 mongoose.connect(uri)
-.then(() => console.log('Conectado CORRECTAMENTE a mongodb, FELICIDADES!!!!'))
+    .then(() => console.log('Conectado CORRECTAMENTE a mongodb, FELICIDADES!!!!'))
     .catch(e => console.log('error de conexión', e))
 
 app.set("view engine", "ejs");
@@ -26,7 +37,7 @@ app.use('/clientes', require('./routers/rutaClientes'))
 
 app.use((req, res, next) => {
     res.status(404).render("404");
-  });
+});
 
 app.listen(puerto, () => {
     console.log(`Servidor activo en el puerto ${puerto}`)
